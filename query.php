d27fc0f28838e5ac54b16676dfaa2639726a5e26
JC <candelariajr@appstate.edu>
2017-01-19 12:56:08 -0500

    Update query.php

--- query.php
+++ query.php
@@ -5,24 +5,24 @@
  * Date: 1/17/2017
  * Time: 1:21 PM
  */
+
+
 require "dbauth.php";
-
-
+$connectString = "host=$host port=$port dbname=$database user=$user password=$password";
 if(isset($_GET['oclcNumber'])){
-    $oclcNum = preg_replace("/\D/", "", $_GET['oclcNumber']);
+	$oclcNum = preg_replace("/\D/", "", $_GET['oclcNumber']);
 }
-
 try{
-    $connec = new PDO("pgsql:host=$host;dbname=$database;port=$port;sslmode=require", $user, $password);
+$connec = new PDO("pgsql:host=$host;dbname=$database;port=$port;sslmode=require", $user, $password);
 }catch (PDOException $e) {
     echo "Error : " . $e->getMessage() . "<br/>";
     die();
 }
 $sql = "
-    SELECT 
-      'b' || record_num AS bib_record_num 
-    FROM 
-      sierra_view.bib_view 
+    SELECT
+      'b' || record_num AS bib_record_num
+    FROM
+      sierra_view.bib_view
     WHERE id = (
         SELECT
 		  record_id
@@ -30,10 +30,9 @@ $sql = "
 		  sierra_view.varfield
 	    WHERE
 		  marc_tag = '001' AND field_content IN ('$oclcNum')
-	LIMIT 1)";
+	LIMIT 1);";
 foreach ($connec->query($sql) as $row)
 {
     print $row['bib_record_num'] . " ";
 }
-
 $connec=null;
